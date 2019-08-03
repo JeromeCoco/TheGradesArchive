9b2e1fd2acadfafb7f0be4f6f26d4ca326812755
JeromeCoco <jeromcoco1@gmail.com>
2019-08-03 22:50:30 +0800

    Log in layout done

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="home-login-wrapper">
     <div class="login-form-wrapper">
         <div class="login-form-title">The Grades Archive</div>
-        <div class="login-form-subtitle">Log In</div>
+        <div class="login-form-subtitle">Log in to your channel.</div>
         <div class="login-field-wrapper">
             <div class="field-label">Username</div>
             <div class="field-input">
@@ -14,6 +14,18 @@
             <div class="field-label">Password</div>
             <div class="field-input">
                 <input type="password" name="password">
+            </div>
+        </div>
+        <div class="login-field-wrapper">
+            <div class="field-input login-submit-wrapper">
+                <button type="button">Log In</button>
+            </div>
+        </div>
+        <div class="sign-up-option-wrapper">
+            <hr>
+            <div class="sign-up-text">OR</div>
+            <div class="sign-up-link">
+                <a href="#" title="Create your channel now!">Create a New Channel!</a>
             </div>
         </div>
     </div>
